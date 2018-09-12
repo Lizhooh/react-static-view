ff6c31d9dc30afbac21e855bd0d1d5835360fd7f
lizhooh <960316408@qq.com>
2018-09-12 09:35:57 +0800

    update: index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,12 @@
 import * as React from 'react';
 
 interface IProps {
-    element: React.ReactElement<HTMLElement | any>,
-    rendered: number | boolean | Array<number>,
+    element?: any,
+    rendered?: number | boolean | Array<number>,
 
-    children: React.ReactChild,
+    children?: React.ReactChild,
 }
 
-export default class ReactStaticView extends React.Component<IProps> { }
+export default class ReactStaticView extends React.Component<IProps> {
+
+}
